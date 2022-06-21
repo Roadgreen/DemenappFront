e3e32813e6fafd614fb7c6ee7fb075717258c9cb
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-06-21 13:51:27 +0200

    master

--- src/components/DashboardAdmin/AdminComponent/AdminUser.js
+++ src/components/DashboardAdmin/AdminComponent/AdminUser.js
@@ -154,17 +154,7 @@ const rows = userWait.map(x => {
   return (
    <>
    
-   <MapContainer style={{height: 250, width: "100%"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+   
 
   <div style={{ height: 400, width: '100%' }}>
   
